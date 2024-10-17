1fda3b9396f5172a0cb65713242ed63ea82c26d7
mayurbodhare <101780046+mayurbodhare@users.noreply.github.com>
2024-10-17 17:29:46 +0000

    why-choose-us component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUS from "@/components/WhyChooseUS";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bd-grid-white/[0.02]">
       <HeroSection />
       <FeaturedSection />
+      <WhyChooseUS />
     </main>
   );
 }
