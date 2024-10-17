f870e7f7d0574ec7122776f9fb95d2c0ec3b5bca
mayurbodhare <101780046+mayurbodhare@users.noreply.github.com>
2024-10-17 03:11:32 +0000

    hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bd-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">
-        Next Started ...
-      </h1>
+      <HeroSection />
     </main>
   );
 }
